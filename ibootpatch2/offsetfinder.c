b00d8de9b89832a8e3f5b8e5ef00a6fc7de8b5ea
ねむい <42214504+dora2-iOS@users.noreply.github.com>
2022-12-18 23:41:54 +0900

    header

--- ibootpatch2/offsetfinder.c
+++ ibootpatch2/offsetfinder.c
@@ -1,15 +1,31 @@
-//
-//  patchfinder64.c
-//  kokeshidoll
-//
-//  Created by Vladimir Putin on 14.12.16.
-//  Fixes by Alex Hude and Max Bazaliy
-//  Updated by sakuRdev on 2021/12/02.
-//  Some parts of code from Luca Todesco and Pangu
-//
-//  Copyright (c) 2016 -2017 FriedApple Team. All rights reserved.
-//  Copyright (c) 2021 sakuRdev. All rights reserved.
-//
+/*
+ * patchfinder64.c
+ *
+ * Created by Vladimir Putin on 14.12.16.
+ * Fixes by Alex Hude and Max Bazaliy
+ * Updated by sakuRdev on 2021/12/02.
+ * Some parts of code from Luca Todesco and Pangu
+ *
+ * Copyright (c) 2016 -2017 FriedApple Team.
+ * Copyright (c) 2021 sakuRdev.
+ * Copyright (c) 2022 dora2ios.
+ *
+ * This file is part of kokeshidoll.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <stdlib.h>
 #include <stdint.h>
